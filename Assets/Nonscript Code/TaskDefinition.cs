c0222574b1ad49a5fa715b057b35f6cb784bba25
yoyokazoo <peter.beckfield@gmail.com>
2021-04-07 14:01:51 -0700

    Stubbed in task loading

---
+++ Assets/Nonscript Code/TaskDefinition.cs
@@ -0,0 +1,7 @@
+﻿public class TaskDefinition
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+
+}
---
+++ Assets/Scripts/TaskDefinitionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TaskDefinitionScript : MonoBehaviour
+{
+    // make non-public once I figure out the loading flow
+    public string Name;
+    // public ??? Image;
+    public string Description;
+
+    public TextMeshProUGUI TaskNameText;
+    public Image TaskImage;
+    public TextMeshProUGUI TaskDescriptionText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TaskNameText.text = Name;
+        TaskDescriptionText.text = Description;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
